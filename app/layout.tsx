8818fcebb1eaf8497928556b353ba771f7e76a9e
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2024-01-13 02:08:58 +0530

    routes added and schemas updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider, auth } from '@clerk/nextjs'
 
 
 export const metadata: Metadata = {
@@ -13,6 +13,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
 
     <ClerkProvider> 
