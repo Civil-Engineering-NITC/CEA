cf498073d3dd5174c3e56dd2b5f33002a8e7569b
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2024-01-24 01:43:19 +0530

    added styled-components to coloured text

--- components/ColouredText.tsx
+++ components/ColouredText.tsx
@@ -1,17 +1,27 @@
-import React from "react";
-import styles from "./colouredText.module.css";
+import React from 'react';
+import styled from 'styled-components';
 
 interface ColouredTextProps {
   text: string;
   size: string;
 }
 
+const ColorTextWrapper = styled.div`
+  font-size: 5rem;
+
+  @media (max-width: 1220px) {
+    font-size: 4rem;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
+
+  @media (max-width: 550px) {
+    font-size: 2rem;
+  }
+`;
+
 export const ColouredText: React.FC<ColouredTextProps> = ({ text, size }) => {
-  return (
-    <>
-      <div className={styles.colorText} style={{ fontSize: size }}>
-        {text}
-      </div>
-    </>
-  );
+  return <ColorTextWrapper>{text}</ColorTextWrapper>;
 };
