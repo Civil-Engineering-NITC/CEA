9be5a0d59064589a79f09ad501d1981c8078ae87
Prashant Kumar <prashantkumarbr@gmail.com>
2024-01-28 13:28:34 +0530

    changes in ourExperience page heading

--- components/sections/OurExperience.tsx
+++ components/sections/OurExperience.tsx
@@ -3,22 +3,24 @@ import styles from "./ourExperience.module.css";
 import { OurExperienceCard } from "../OurExperienceCard";
 import { DownArrowButton } from "../DownArrowButton";
 import { ColouredText } from "../ColouredText";
+import { Header } from "../Header";
 
 export const OurExperience = () => {
   return (
     <>
       <div className={styles.mainDiv}>
         <div className={styles.topDiv}>
-          <div className={styles.headingDiv}>
-            <h1>Our Experience</h1>
+          <Header headingText="Activities" subHeadingText="Your Action" />
+          {/* <div className={styles.headingDiv}>
+            <h1>Activities Activities Activities</h1>
             <h2>Your Action</h2>
-          </div>
-          <p>
+          </div> */}
+          {/* <p>
             Having gained extensive experience various companies, we've curated
             something unique for you. Delve into our portfolio to witness some
             of our latest experiences and delve deeper into our expertise and
             capabilities.
-          </p>
+          </p> */}
         </div>
         <div className={styles.cardMainDiv}>
           <OurExperienceCard />
