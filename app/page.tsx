5a0938e7a3cad112110c6f107d6a782d3e8832ab
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2024-01-30 15:00:33 +0530

    convert page.tsx client --> server

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { InterviewExp } from "@/components/sections/InterviewExp";
-import { UserButton, useUser } from "@clerk/nextjs";
+import { UserButton, currentUser, useUser } from "@clerk/nextjs";
 import styles from "./page.module.css";
 import { Resource } from "@/components/sections/Resource";
 import { HomePage } from "@/components/sections/HomePage";
@@ -17,31 +15,13 @@ import Resources from "./(routes)/resources/page";
 
 // import { Competitive } from "@/components/sections/Competitive";
 
-export default function Home() {
-  const { isSignedIn, user, isLoaded } = useUser();
-
-  if (!isLoaded) {
-    return (
-      <>
-        <h1>USER IS NOT LOADED</h1>
-      </>
-    );
-  }
-
-  if (isSignedIn) {
-    return (
-      <>
-<<<<<<< HEAD
-        {/* <div>Hello {user.emailAddresses[0].emailAddress}!</div>
-      <UserButton afterSignOutUrl="/"/> */}
-        <Footer />
-=======
-        <div>Hello {user.emailAddresses[0].emailAddress}!</div>
-        <UserButton afterSignOutUrl="/" />
->>>>>>> b022ec21e7c4f59ecba1203fcf8e9f03cb7a150a
-      </>
-    );
-  }
-
-  return <div>Not signed in</div>;
+export default async function Home() {
+  // const { isSignedIn, user, isLoaded } = useUser();
+  const user = await currentUser();
+  return (
+    <>
+      {/* <div>Hello {user?.emailAddresses[0].emailAddress}!</div> */}
+      <UserButton afterSignOutUrl="/" />
+    </>
+  );
 }
